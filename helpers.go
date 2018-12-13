7ef6595289d3c760eb3054f63ba2ddbf5a96f4d4
jtamsut <jtamsut1993@gmail.com>
2018-12-12 17:18:37 -0800

    add dict mapper

--- helpers.go
+++ helpers.go
@@ -9,6 +9,11 @@ import (
 	"github.com/pkg/errors"
 )
 
+// WrapError ... handles errors
+func WrapError(e error) {
+	log.Fatal(errors.Wrap(e, "Couldn't parse input"))
+}
+
 // BreakOnNewLines ... takes a file and returns a slice of strings
 func BreakOnNewLines(fileName string) ([]string, error) {
 	file, err := ioutil.ReadFile(fileName)
@@ -25,7 +30,7 @@ func BreakOnNewLines(fileName string) ([]string, error) {
 func ParseIntList(fileName string) []int64 {
 	s, err := BreakOnNewLines(fileName)
 	if err != nil {
-		log.Fatal(errors.Wrap(err, "Couldn't parse input"))
+		WrapError(err)
 
 	}
 	var d []int64
@@ -46,4 +51,36 @@ func Sum(lst ...int64) int64 {
 		sum += val
 	}
 	return sum
+}
+
+// LettersToNums ... returns map that maps letter to number
+func LettersToNums() map[string]int {
+	return map[string]int{
+		"a": 0,
+		"b": 1,
+		"c": 2,
+		"d": 3,
+		"e": 4,
+		"f": 5,
+		"g": 6,
+		"h": 7,
+		"i": 8,
+		"j": 9,
+		"k": 10,
+		"l": 11,
+		"m": 12,
+		"n": 13,
+		"o": 14,
+		"p": 15,
+		"q": 16,
+		"r": 17,
+		"s": 18,
+		"t": 19,
+		"u": 20,
+		"v": 21,
+		"w": 22,
+		"x": 23,
+		"y": 24,
+		"z": 25,
+	}
 }
