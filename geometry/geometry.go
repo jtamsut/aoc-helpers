4a2fbb8bf08eafda8b6377738ee022be95e9e626
jtamsut <jtamsut1993@gmail.com>
2018-12-23 10:43:39 -0800

    make struct properties global

--- geometry/geometry.go
+++ geometry/geometry.go
@@ -4,15 +4,15 @@ import helpers "github.com/jtamsut/aochelpers"
 
 // Box ... represents the coordinates of a box
 type Box struct {
-	leftX, topY, width, height int
+	LeftX, TopY, Width, Height int
 }
 
 // DetermineOverlap ... determines if there is an overlap between two boxes
 func DetermineOverlap(p Box, m Box) (bool, Box) {
-	lX := helpers.Max(p.leftX, m.leftX)
-	rX := helpers.Min(p.leftX+p.width, m.leftX+m.width)
-	bY := helpers.Max(p.topY-p.height, m.topY-m.height)
-	tY := helpers.Min(p.topY, m.topY)
+	lX := helpers.Max(p.LeftX, m.LeftX)
+	rX := helpers.Min(p.LeftX+p.Width, m.LeftX+m.Width)
+	bY := helpers.Max(p.TopY-p.Height, m.TopY-m.Height)
+	tY := helpers.Min(p.TopY, m.TopY)
 	var boxWidth, boxHeight int
 
 	if rX > lX {
@@ -30,9 +30,9 @@ func DetermineOverlap(p Box, m Box) (bool, Box) {
 
 	// if overlap, return false and resulting overlapping box
 	return true, Box{
-		leftX:  lX,
-		topY:   tY,
-		width:  boxWidth,
-		height: boxHeight,
+		LeftX:  lX,
+		TopY:   tY,
+		Width:  boxWidth,
+		Height: boxHeight,
 	}
 }
